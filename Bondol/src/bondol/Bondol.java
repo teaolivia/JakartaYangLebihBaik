c7cb0e0b0f7fd56fdc532aa814a8f38d22e38740
atianadya <theaolivia.mail@gmail.com>
2016-11-11 13:57:32 +0700

    trying to retrieve Twitter posts - dunno why the error occurs though

--- Bondol/src/bondol/Bondol.java
+++ Bondol/src/bondol/Bondol.java
@@ -9,13 +9,29 @@ package bondol;
  *
  * @author theaolivia
  */
+
+
+import twitter4j.*;
+import twitter4j.Query;
+
+
 public class Bondol {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // make requests to Twitter
+        Twitter twitter = TwitterFactory.getSingleton();
+        
+        Query query;
+        query = new Query("source:twitter4j yusukey");
+        
+        QueryResult result = twitter.search(query);
+        
+        result.getTweets().stream().forEach((status) -> {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        });
     }
     
 }
